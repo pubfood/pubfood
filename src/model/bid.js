e385cc9c06138317b52505b982dd19423982243b
Andrei McMillan <amcmillan@yieldbot.com>
2015-10-12 16:24:59 -0400

    update docs for bid

--- src/model/bid.js
+++ src/model/bid.js
@@ -24,26 +24,41 @@ function Bid() {
 /**
  * Create a new [Bid]{@link pubfood/model.Bid} from an object.
  *
- * @param {object} config - bid object literal
- * @returns {object} instance of [Bid]{@link pubfood/model.Bid}
+ * @param {object} config bid object literal
+ * @returns {pubfood/model.Bid} instance of [Bid]{@link pubfood/model.Bid}
  */
 Bid.fromObject = function(config) {
 
 };
 
-
+/**
+ * Sets the bid value
+ * @param {string|number} v
+ * @return {pubfood/model.Bid}
+ */
 Bid.prototype.value = function(v) {
   this.value = v || '';
   this.type = util.asType(this.value);
   return this;
 };
 
+/**
+ * Set the bid's slot
+ * @param {pubfood/model.Slot} s
+ * @return {pubfood/model.Bid}
+ */
 Bid.prototype.slot = function(s) {
   this.slot = s;
   return this;
 };
 
 /*jslint bitwise: true */
+/**
+ * Set's the ad
+ * @param {string|number} w
+ * @param {string|number} h
+ * @return {pubfood/model.Bid}
+ */
 Bid.prototype.addSize = function(w, h) {
   var width = Math.abs(~~w);
   var height  = Math.abs(~~h);
@@ -51,10 +66,14 @@ Bid.prototype.addSize = function(w, h) {
   this.sizes.push([width, height]);
 
   return this;
-
 };
 /*jslint bitwise: false */
 
+/**
+ * Sets the bid's provider
+ * @param {pubfood/provider.BidProvider} p
+ * @return {pubfood/model.Bid}
+ */
 Bid.prototype.provider = function(p) {
   this.provider = p;
   return this;
