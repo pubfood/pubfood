71dc8d07eacf91eded5d3057fa83b79560228453
Andrei McMillan <amcmillan@yieldbot.com>
2015-10-28 18:22:46 -0400

    copy build files to dist

--- dist/pubfood.js
+++ dist/pubfood.js
@@ -2120,7 +2120,7 @@ var logger = require('./logger');
   };
 
   pubfood.library = pubfood.prototype = {
-    version: '0.1.0',
+    version: '0.1.2',
     mediator: require('./mediator').mediatorBuilder(),
     PubfoodError: require('./errors'),
     logger: logger
