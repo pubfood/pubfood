9fcf57000df9af112ca3c1a50806cfb2d954c549
John W. Ellis <jellis@yieldbot.com>
2016-01-13 19:00:31 -0500

    fix: emit BID_PUSH_NEXT_LATE

--- test/mediator/auctionmediatorlatebids.js
+++ test/mediator/auctionmediatorlatebids.js
@@ -78,6 +78,61 @@ describe('Late bids', function () {
     m.start();
   });
 
+  it('should emit a BID_PUSH_NEXT_LATE event', function(done) {
+
+    var m = new AuctionMediator();
+
+    m.addSlot({
+      name: '/abc/123',
+      sizes: [
+        [728, 90]
+      ],
+      elementId: 'div-leaderboard',
+      bidProviders: ['b1']
+    });
+
+    m.addBidProvider({
+      name: 'b1',
+      libUri: '../test/fixture/lib.js',
+      init: function(slots, pushBid, done) {
+        var argPushBid = pushBid;
+        setTimeout(function() {
+          argPushBid({
+            slot: '/abc/123',
+            value: '445',
+            sizes: [728, 90]
+          });
+          done();
+        }, 5);
+      },
+      refresh: function(slots, pushBid, done) {
+        done();
+      }
+    });
+
+    m.setAuctionProvider({
+      name: 'provider1',
+      libUri: '../test/fixture/lib.js',
+      init: function(targeting, done) {
+        done();
+      },
+      refresh: function(targeting, done) {
+        done();
+      }
+    });
+
+    Event.on(Event.EVENT_TYPE.BID_PUSH_NEXT_LATE, function() {
+      var lateBids = m.getAuctionRunLateBids(1);
+      assert.equal(lateBids.length, 1, 'should be one late bid');
+      var lateBid = lateBids[0];
+      assert.equal(lateBid.value, 445, 'should be a bid for 445');
+
+      done();
+    });
+    m.timeout(1);
+    m.start();
+  });
+
   it('should keep timely bids separate from late bids', function(done) {
 
     var m = new AuctionMediator();
