d9d6c0de746d2fa5aef84285167f9aaebd6fb2c1
John W. Ellis <jellis@yieldbot.com>
2016-01-22 16:58:30 -0500

    copy build files to dist

--- dist/pubfood.js
+++ dist/pubfood.js
@@ -1,4 +1,4 @@
-/*! pubfood v0.1.13 | (c) pubfood | http://pubfood.org/LICENSE.txt */
+/*! pubfood v0.1.14 | (c) pubfood | http://pubfood.org/LICENSE.txt */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.pubfood = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
@@ -1793,7 +1793,7 @@ AuctionMediator.prototype.refresh = function(slotNames) {
  * @param {BidderSlots[]} bidderSlots object containing slots per bidder
  */
 AuctionMediator.prototype.processBids = function(auctionIdx, auctionType, bidderSlots) {
-  for (var k in bidderSlots) {
+  for (var k = 0; k < bidderSlots.length; k++) {
     this.getBids_(auctionIdx, auctionType, bidderSlots[k].provider, bidderSlots[k].slots);
   }
 };
@@ -1856,11 +1856,12 @@ AuctionMediator.prototype.pushBid = function(auctionIdx, bidObject, providerName
   var bid = Bid.fromObject(bidObject);
   if (bid) {
     bid.provider = providerName;
-    Event.publish(Event.EVENT_TYPE.BID_PUSH_NEXT, bid);
     if (!this.auctionRun[auctionIdx].inAuction) {
       this.auctionRun[auctionIdx].bids.push(bid);
+      Event.publish(Event.EVENT_TYPE.BID_PUSH_NEXT, bid);
     } else {
       this.auctionRun[auctionIdx].lateBids.push(bid);
+      Event.publish(Event.EVENT_TYPE.BID_PUSH_NEXT_LATE, bid);
     }
   } else {
     Event.publish(Event.EVENT_TYPE.WARN, 'Invalid bid object: ' + JSON.stringify(bidObject || {}));
@@ -2415,7 +2416,7 @@ var AuctionMediator = require('./mediator/auctionmediator');
   };
 
   pubfood.library = pubfood.prototype = {
-    version: '0.1.13',
+    version: '0.1.14',
     PubfoodError: require('./errors'),
     logger: logger
   };
@@ -2866,8 +2867,7 @@ var util = {
 
     child.prototype.init_ = function() {
       var parents = this.parents || [];
-
-      for (var i in parents) {
+      for (var i = 0; i < parents.length; i++) {
         parents[i]().call(this);
       }
     };
